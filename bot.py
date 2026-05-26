fcea130192ceed089c14afd8e8c3be9a77684059
orionteam26 <orionteam26@gmail.com>
2026-05-26 12:27:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
-RSS_FEED = "https://www.forexfactory.com/ffcal_week_this.xml"
+RSS_FEED = "https://rss.fxstreet.com/latest-news"
 
 def send_to_discord(message):
     requests.post(DISCORD_WEBHOOK_URL, json={"content": message})
@@ -21,14 +21,21 @@ def main():
 
     message = f"📰 NOTIZIE FOREX DEL GIORNO - {today}\n\n"
 
-    for entry in feed.entries[:5]:
+    count = 0
+
+    for entry in feed.entries:
 
         title = entry.title
         link = entry.link
 
         message += f"🔹 {title}\n{link}\n\n"
 
-    message += "⚠️ Fonte: MyFxBook"
+        count += 1
+
+        if count >= 5:
+            break
+
+    message += "⚠️ Fonte: FXStreet"
 
     send_to_discord(message)
 
